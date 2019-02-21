d0a868bd3d699e69363d3fabddf15d67c5d6a861
Rohan Das <rohandasbirbhum@gmail.com>
2019-02-21 20:18:23 +0530

    Initial Commit

---
+++ Using Concatenating Strings.py
@@ -0,0 +1,10 @@
+#Using Concatenating Strings 
+for num in range(1,101):
+    string = ""
+    if num % 3 == 0:
+        string = string + "Fizz"
+    if num % 5 == 0:
+        string = string + "Buzz"
+    if num % 5 != 0 and num % 3 != 0:
+        string = string + str(num)
+    print(string)
---
+++ Using if, elif, else.py
@@ -0,0 +1,10 @@
+#We can also use if, elif, and else
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print('FizzBuzz')
+    elif num % 3 == 0:
+        print('Fizz')
+    elif num % 5 == 0:
+        print('Buzz')
+    else:
+        print(num)
